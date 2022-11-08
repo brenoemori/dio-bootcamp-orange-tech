8f3a091842548ff6675566fa038ee5714336a469
brenoemori <contatobrenoemori@gmail.com>
2022-11-07 23:35:21 -0300

    exercícios completos de arrays

--- Javascript/06 - arrays/listaExercicios.js
+++ Javascript/06 - arrays/listaExercicios.js
@@ -12,6 +12,14 @@
 // 6) Dado uma lista com as notas tiradas pelos alunos. imprima a maior nota.
 //   Ex de lista de notas = [2, 7, 3, 8, 10, 4]
 
+const numerosPares = [];
+
+    for (let i = 0; i < 10; i = i + 2) {
+        numerosPares.push(i);
+    }
+
+    console.log(numerosPares);
+
 
 
 
